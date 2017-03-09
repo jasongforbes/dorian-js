18a73738784c5bc38ca833382efd5387d0adf866
Jason Forbes <jason.g.forbes@gmail.com>
2017-03-09 19:24:22 -0330

    Pass post info to PostScroller. Posts saved to media/posts folder. Posts can be keyed by their slugified title. Posts are loaded when document is less than twice window (loading is faked as of this commit).

--- src/App.js
+++ src/App.js
@@ -1,36 +1,97 @@
 import React from 'react';
+import slugify from 'slugify';
 import Header from './components/Header';
 import PostScroller from './components/PostScroller';
 import { importAll } from './utility';
 import avatar from '../images/avatar.svg';
 
 const postFrontMatter = importAll(require.context('!json!./loaders/frontmatter-loader?expected[]=date,expected[]=author,expected[]=title!../posts/', true, /\.md$/));
-const postFile = importAll(require.context('!file?name=posts/[name].html!./loaders/markdown-loader!../posts/', true, /\.md$/));
+const postFile = importAll(require.context('!file?name=media/posts/[name].[hash].html!./loaders/markdown-loader!../posts/', true, /\.md$/));
 
 class App extends React.Component {
   constructor() {
     super();
     this.handleLoadPosts = this.handleLoadPosts.bind(this);
+    this.getPostsToLoad = this.getPostsToLoad.bind(this);
+    this.loadPost = this.loadPost.bind(this);
+    this.getPost = this.getPost.bind(this);
+    this.getPostOrdering = this.getPostOrdering.bind(this);
+
     const posts = postFrontMatter.map((e, i) => ({
       frontMatter: e,
       file: postFile[i],
       loaded: false,
       body: null,
-    })).filter(post => post.frontMatter.isValid);
+    }))
+      .filter(post => post.frontMatter.isValid)
+      // sort by title so similar titles are slugified in order with proper titles
+      .sort((a, b) => {
+        if (a.frontMatter.title.toLowerCase() < b.frontMatter.title.toLowerCase()) {
+          return -1;
+        } else if (a.frontMatter.title.toLowerCase() === b.frontMatter.title.toLowerCase()) {
+          return a.frontMatter.date < b.frontMatter.date ? -1 : 1;
+        }
+        return 1;
+      })
+      .reduce((origPosts, post) => {
+        const modifiedPosts = origPosts;
+        const title = slugify(post.frontMatter.title.toLowerCase());
+        if (title in modifiedPosts) {
+          // Duplicate post-title ... append an integer instead of failing
+          // Use slugified titles as keys as they must be unique (otherwise will fail to redirect)
+          const postRegex = new RegExp(`^${title}(-[\\d]+)?$`, 'i');
+          const uniqueID = Object.keys(modifiedPosts).filter(key => postRegex.test(key)).length;
+          modifiedPosts[`${title}-${uniqueID}`] = post;
+        } else {
+          modifiedPosts[title] = post;
+        }
+        return modifiedPosts;
+      }, {});
+
     this.state = {
       posts,
     };
   }
 
-  componentDidMount() {
-    this.handleLoadPosts();
+  getPost(key) {
+    return this.state.posts[key];
+  }
+
+  getPostsToLoad() {
+    return Object.keys(this.state.posts)
+      .filter(key => !this.state.posts[key].loaded)
+      .sort(this.getPostOrdering);
+  }
+
+  getPostOrdering(key1, key2) {
+    return (this.state.posts[key1].frontMatter.date <
+            this.state.posts[key2].frontMatter.date ? 1 : -1);
   }
 
   handleLoadPosts() {
-    console.log('loading posts');
+    const posts = this.state.posts;
+    const numPostsToLoad = 1;
+    const postsToLoad = this.getPostsToLoad();
+    if (postsToLoad.length > 0) {
+      postsToLoad.slice(0, numPostsToLoad).forEach((key) => {
+        posts[key] = this.loadPost(key);
+      });
+      this.setState({
+        posts,
+      });
+    }
+  }
+
+  loadPost(key) {
+    const post = this.state.posts[key];
+    post.loaded = true;
+    return post;
   }
 
   render() {
+    const loadedPosts = Object.keys(this.state.posts)
+      .filter(key => this.state.posts[key].loaded)
+      .sort(this.getPostOrdering);
     return (
       <div className="app">
         <Header
@@ -40,8 +101,9 @@ class App extends React.Component {
           description="This is the landing page description."
         />
         <PostScroller
-          postsToLoad
+          hasPostsToLoad={loadedPosts.length < Object.keys(this.state.posts).length}
           onLoadPosts={this.handleLoadPosts}
+          posts={loadedPosts}
         />
       </div>
     );
--- src/components/PostScroller.js
+++ src/components/PostScroller.js
@@ -2,44 +2,64 @@ import React, { Component } from 'react';
 
 class PostScroller extends Component {
 
+  static renderPostSummary(key) {
+    return <p key={key}>{key}</p>;
+  }
+
+  static isDocumentSmallerThanTwiceWindow() {
+    const windowHeight = window.innerHeight || document.documentElement.offsetHeight;
+    const docHeight = Math.max(document.body.scrollHeight,
+                              document.body.offsetHeight,
+                              document.documentElement.clientHeight,
+                              document.documentElement.scrollHeight,
+                              document.documentElement.offsetHeight);
+    const windowBottom = windowHeight + window.pageYOffset;
+    return docHeight < 2 * windowBottom;
+  }
+
   constructor(props) {
     super(props);
-    this.handleScroll = this.handleScroll.bind(this);
+    this.handleViewChange = this.handleViewChange.bind(this);
   }
 
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener('scroll', this.handleViewChange);
+    window.addEventListener('resize', this.handleViewChange);
+    this.props.onLoadPosts();
+  }
+
+  componentDidUpdate() {
+    if (this.props.hasPostsToLoad &&
+        PostScroller.isDocumentSmallerThanTwiceWindow()) {
+      this.props.onLoadPosts();
+    }
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener('scroll', this.handleViewChange);
+    window.removeEventListener('resize', this.handleViewChange);
   }
 
-  handleScroll() {
-    if (this.props.postsToLoad) {
-      const windowHeight = window.innerHeight || document.documentElement.offsetHeight;
-      const docHeight = Math.max(document.body.scrollHeight,
-                                document.body.offsetHeight,
-                                document.documentElement.clientHeight,
-                                document.documentElement.scrollHeight,
-                                document.documentElement.offsetHeight);
-      const windowBottom = windowHeight + window.pageYOffset;
-      if (windowBottom >= docHeight) {
-        this.props.onLoadPosts();
-      }
+  handleViewChange() {
+    if (this.props.hasPostsToLoad &&
+        PostScroller.isDocumentSmallerThanTwiceWindow()) {
+      this.props.onLoadPosts();
     }
   }
 
   render() {
     return (
-      <div className="post-scroller" />
+      <div className="post-scroller">
+        {this.props.posts.map(PostScroller.renderPostSummary)}
+      </div>
     );
   }
 }
 
 PostScroller.propTypes = {
-  postsToLoad: React.PropTypes.bool.isRequired,
+  hasPostsToLoad: React.PropTypes.bool.isRequired,
   onLoadPosts: React.PropTypes.func.isRequired,
+  posts: React.PropTypes.array.isRequired,
 };
 
 export default PostScroller;
