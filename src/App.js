5ed337e7aaa384e17bd4dedfcb8df939783940a3
Jason Forbes <jason.g.forbes@gmail.com>
2017-03-21 21:25:54 -0230

    Pages are loaded from md files. A sample About page is added.
    Moved common load functionality.
    Moved the markdown loader functionality to the utility function.

--- src/App.js
+++ src/App.js
@@ -1,79 +1,90 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import slugify from 'slugify';
 import Header from './components/Header';
 import PostScroller from './components/PostScroller';
 import NotFound from './components/NotFound';
 import Post from './components/Post';
-import { importAll } from './utility';
+import { importAll, loadMarkdown } from './utility';
 import avatar from '../images/avatar.svg';
 
-const postFrontMatter = importAll(require.context('!json!./loaders/frontmatter-loader?expected[]=date,expected[]=title!../posts/', true, /\.md$/));
-const postFile = importAll(require.context('!file?name=media/posts/[name].[hash].html!./loaders/markdown-loader!../posts/', true, /\.md$/));
 
 class App extends React.Component {
+  static getData(key, collection, handleReturn, handleError) {
+    if (collection[key].body) {
+      handleReturn(key, collection.posts[key].body);
+    }
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = () => {
+      if (request.readyState === 4) { // Done
+        if (request.status === 200) { // OK
+          handleReturn(key, request.responseText);
+        } else if (handleError) {
+          handleError(key);
+        }
+      }
+    };
+    request.open('GET', collection[key].file, true);
+    request.send(null);
+  }
+
+  static updateFile(collection, key, response) {
+    const modifiedCollection = collection;
+    if (modifiedCollection[key].isLoading) {
+      modifiedCollection[key].loaded = true;
+      modifiedCollection[key].isLoading = false;
+    }
+    modifiedCollection[key].body = response;
+    return modifiedCollection;
+  }
+
   constructor() {
     super();
     this.handleLoadPosts = this.handleLoadPosts.bind(this);
     this.getPostsToLoad = this.getPostsToLoad.bind(this);
+    this.loadPage = this.loadPage.bind(this);
     this.loadPost = this.loadPost.bind(this);
     this.getPost = this.getPost.bind(this);
+    this.getPostData = this.getPostData.bind(this);
     this.getPostOrdering = this.getPostOrdering.bind(this);
-    this.getData = this.getData.bind(this);
+    this.getPage = this.getPage.bind(this);
+    this.getPageData = this.getPageData.bind(this);
 
-    const posts = postFrontMatter.map((e, i) => ({
-      frontMatter: e,
-      file: postFile[i],
-      loaded: false,
-      isLoading: false,
-      body: null,
-    }))
-      .filter(post => post.frontMatter.isValid)
-      // sort by title so similar titles are slugified in order with proper titles
-      .sort((a, b) => {
+    const posts = loadMarkdown(
+      importAll(require.context('!json!./loaders/frontmatter-loader?expected[]=date,expected[]=title!../posts/', true, /\.md$/)),
+      importAll(require.context('!file?name=media/posts/[name].[hash].html!./loaders/markdown-loader!../posts/', true, /\.md$/)),
+      (a, b) => {
         if (a.frontMatter.title.toLowerCase() < b.frontMatter.title.toLowerCase()) {
           return -1;
         } else if (a.frontMatter.title.toLowerCase() === b.frontMatter.title.toLowerCase()) {
           return a.frontMatter.date < b.frontMatter.date ? -1 : 1;
         }
         return 1;
-      })
-      .reduce((origPosts, post) => {
-        const modifiedPosts = origPosts;
-        const title = slugify(post.frontMatter.title.toLowerCase());
-        if (title in modifiedPosts) {
-          // Duplicate post-title ... append an integer instead of failing
-          // Use slugified titles as keys as they must be unique (otherwise will fail to redirect)
-          const postRegex = new RegExp(`^${title}(-[\\d]+)?$`, 'i');
-          const uniqueID = Object.keys(modifiedPosts).filter(key => postRegex.test(key)).length;
-          modifiedPosts[`${title}-${uniqueID}`] = post;
-        } else {
-          modifiedPosts[title] = post;
-        }
-        return modifiedPosts;
-      }, {});
+      });
+
+    const pages = loadMarkdown(
+      importAll(require.context('!json!./loaders/frontmatter-loader?expected[]=title,expected[]=order!../pages/', true, /\.md$/)),
+      importAll(require.context('!file?name=media/pages/[name].[hash].html!./loaders/markdown-loader!../pages/', true, /\.md$/)),
+    );
 
     this.state = {
+      pages,
       posts,
     };
+
+    Object.keys(pages).forEach(key =>
+      this.getPageData(key, this.loadPage));
   }
 
-  getData(key, handleReturn, handleError) {
-    if (this.state.posts[key].body) {
-      handleReturn(key, this.state.posts[key].body);
-    }
-    const postRequest = new XMLHttpRequest();
-    postRequest.onreadystatechange = () => {
-      if (postRequest.readyState === 4) { // Done
-        if (postRequest.status === 200) { // OK
-          handleReturn(key, postRequest.responseText);
-        } else if (handleError) {
-          handleError(key);
-        }
-      }
-    };
-    postRequest.open('GET', this.state.posts[key].file, true);
-    postRequest.send(null);
+  getPostData(key, handleReturn, handleError) {
+    return App.getData(key, this.state.posts, handleReturn, handleError);
+  }
+
+  getPageData(key, handleReturn, handleError) {
+    return App.getData(key, this.state.pages, handleReturn, handleError);
+  }
+
+  getPage(key) {
+    return this.state.pages[key];
   }
 
   getPost(key) {
@@ -99,19 +110,17 @@ class App extends React.Component {
         const posts = this.state.posts;
         posts[key].isLoading = true;
         this.setState({ posts });
-        this.getData(key, this.loadPost);
+        this.getPostData(key, this.loadPost);
       });
     }
   }
 
+  loadPage(key, response) {
+    this.setState({ pages: App.updateFile(this.state.pages, key, response) });
+  }
+
   loadPost(key, response) {
-    const posts = this.state.posts;
-    if (posts[key].isLoading) {
-      posts[key].loaded = true;
-      posts[key].isLoading = false;
-    }
-    posts[key].body = response;
-    this.setState({ posts });
+    this.setState({ posts: App.updateFile(this.state.posts, key, response) });
   }
 
   render() {
@@ -147,7 +156,7 @@ class App extends React.Component {
               component={({ match }) => {
                 if (match.params.title in this.state.posts) {
                   if (!this.state.posts[match.params.title].body) {
-                    this.getData(match.params.title, this.loadPost);
+                    this.getPostData(match.params.title, this.loadPost);
                   }
                   return (
                     <Post
--- src/utility.js
+++ src/utility.js
@@ -1,3 +1,5 @@
+import slugify from 'slugify';
+
 const payloadRegex = /^data:text\/([\w-]+);(?:([\w\d]+),)?(.*)$/;
 
 export function parseBase64(string) {
@@ -20,4 +22,32 @@ export function parseMarkdownPayload(payload) {
 
 export function importAll(requireContext) {
   return requireContext.keys().map(requireContext);
+}
+
+export function loadMarkdown(frontMatter, fileNames, sortFunc = null) {
+  const files = frontMatter.map((e, i) => ({
+    frontMatter: e,
+    file: fileNames[i],
+    loaded: false,
+    isLoading: false,
+    body: null,
+  }))
+    .filter(file => file.frontMatter.isValid)
+    // sort by title so similar titles are slugified in order with proper titles
+    .sort(sortFunc)
+    .reduce((origFiles, file) => {
+      const modifiedFiles = origFiles;
+      const title = slugify(file.frontMatter.title.toLowerCase());
+      if (title in modifiedFiles) {
+        // Duplicate title ... append an integer instead of failing
+        // Use slugified titles as keys as they must be unique (otherwise will fail to redirect)
+        const titleRegex = new RegExp(`^${title}(-[\\d]+)?$`, 'i');
+        const uniqueID = Object.keys(modifiedFiles).filter(key => titleRegex.test(key)).length;
+        modifiedFiles[`${title}-${uniqueID}`] = file;
+      } else {
+        modifiedFiles[title] = file;
+      }
+      return modifiedFiles;
+    }, {});
+  return files;
 }
