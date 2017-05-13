449f09b029e46fc947fc0f5f4056f7d0613ad207
Jason Forbes <jason.g.forbes@gmail.com>
2017-05-13 10:35:45 -0230

    Changed markdown parser to include inner-html.

--- src/loaders/markdown-loader.js
+++ src/loaders/markdown-loader.js
@@ -1,7 +1,7 @@
 const FrontMatter = require('front-matter');
-const mkdown = require('markdown');
+const mkdown = require('marked');
 
 module.exports = function load(source) {
   const frontmatter = FrontMatter(source);
-  return mkdown.markdown.toHTML(frontmatter.body);
+  return mkdown(frontmatter.body);
 };
